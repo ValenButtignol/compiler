93180add623f829d5b424de645bdf88fc684fbf2
AFungo <augustofungo@gmail.com>
2023-11-06 12:50:30 -0300

    add function that adds a new error in errors list and amake fuctions thats check ast types with his sons

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -5,5 +5,8 @@
 #include "../dataStructures/nodeInfo.h"
 
 void setParamsToNodeInfo(NodeInfo** node, TAst* ast);
-
+void addOperationError(ErrorNode** errors, int lineNumber, enum TTag tag);
+int checkReturnTypes(TAst *ast);
+int checkSonsTypes(TAst *ast);
+int checkSonsFatherTypes(TAst *ast);
 #endif
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -15,3 +15,23 @@ void setParamsToNodeInfo(NodeInfo** node, TAst* ast){
         
     return ;
 }
+
+void addOperationError(ErrorNode** errors, int lineNumber, enum TTag tag){
+    int errorStrLength = snprintf(NULL, 0, "\033[1;36mLine: %d \033[1;31mError:\033[0m Type mismatch in \'%s\'\n", lineNumber, operatorToString(tag));
+    char* errorStr = malloc(errorStrLength + 1);
+    sprintf(errorStr, "\033[1;36mLine: %d \033[1;31mError:\033[0m Type mismatch in \'%s\'\n", lineNumber, operatorToString(tag));
+    insertErrorNode(errors, errorStr);
+}
+
+int checkReturnTypes(TAst *ast){
+    enum TType lsType = getAstType(ast->ls);
+    return !((lsType==ast->data->type) && (VOID==ast->data->type && ast->ls==NULL && lsType==NONETYPE));
+}
+
+int checkSonsTypes(TAst *ast){
+    return !(getAstType(ast->ls) == getAstType(ast->rs));
+}
+
+int checkSonsFatherTypes(TAst *ast){
+    return !(!checkSonsTypes(ast) && getAstType(ast->ls) == ast->data->type);
+}
