2316d0d32bcfe23832a90f3fbc551a5571bcc93a
AFungo <augustofungo@gmail.com>
2023-11-06 14:02:32 -0300

    Refactor method check types in check types function

--- src/algorithms/checktypes.c
+++ src/algorithms/checktypes.c
@@ -8,13 +8,12 @@ int checkTypes(TAst* ast, ErrorNode** errors) {
     }
 
     return 1;
-}   
+}
 
 void checkForTypeErrors(TAst* ast, ErrorNode** errors) {
 
-    if (ast == NULL || isEmptyAst(*ast)) {
+    if (ast == NULL || isEmptyAst(*ast))
         return;
-    }
 
     if(ast->data->type != NONETYPE){
         enum TType lsType = getAstType(ast->ls);
@@ -22,60 +21,32 @@ void checkForTypeErrors(TAst* ast, ErrorNode** errors) {
         if(isAssignTag(ast->data->tag) || isArithmeticOrBooleanTag(ast->data->tag)){
             if(checkSonsFatherTypes(ast))
                 addOperationError(errors, ast->data->lineNumber, ast->data->tag);
-        }
-        if(isRelationalOperatorTag(ast->data->tag)){
+        }else if(isRelationalOperatorTag(ast->data->tag)){
             if(checkSonsTypes(ast))
                 addOperationError(errors, ast->data->lineNumber, ast->data->tag);
-        }if(isReturnTag(ast->data->tag)){
+        }else if(isReturnTag(ast->data->tag)){
             if(checkReturnTypes(ast)) 
                 addOperationError(errors, ast->data->lineNumber, ast->data->tag);
-        }
-        // printf("TYPE = %s\n", typeToString(ast->data->type));
-        if(ast->data->tag==METHOD_CALL){
-            int methodNameSize = snprintf(NULL, 0, "%s", ast->data->id);
-            char* methodName = malloc(methodNameSize + 1);
-            sprintf(methodName, "%s", ast->rs->data->id);
+        }else if(isMethodCallTag(ast->data->tag)){
+            char* methodName;
+            copyMethodName(&methodName ,ast);
             NodeInfo* currentParameters = ast->data->nextParams;
             NodeInfo* formalParameters = ast->rs->data->nextParams;
+            
             while(currentParameters != NULL && formalParameters != NULL ){
-                if(!(currentParameters->type == formalParameters->type)){
-                    int errorStrLength = snprintf(NULL, 0, "\033[1;36mLine: %d \033[1;31mError:\033[0m Type mismatch in function %s call\n", (ast->data)->lineNumber, methodName);
-                    char* errorStr = malloc(errorStrLength + 1);
-                    sprintf(errorStr, "\033[1;36mLine: %d \033[1;31mError:\033[0m Type mismatch in function %s call\n", (ast->data)->lineNumber, methodName);
-                    insertErrorNode(errors, errorStr);
-                }
+                if(!(currentParameters->type == formalParameters->type))
+                    addMethodCallError(errors, (ast->data)->lineNumber, "Type mismatch", methodName);
                 currentParameters = currentParameters->nextParams;
                 formalParameters = formalParameters->nextParams;
             }
-            if(currentParameters != NULL){//dividir el if para diferenciar el error
-                int errorStrLength = snprintf(NULL, 0, "\033[1;36mLine: %d \033[1;31mError:\033[0m To many parameters in function %s call\n", (ast->data)->lineNumber, methodName);
-                char* errorStr = malloc(errorStrLength + 1);
-                sprintf(errorStr, "\033[1;36mLine: %d \033[1;31mError:\033[0m To many parameters in function %s call\n", (ast->data)->lineNumber, methodName);
-                insertErrorNode(errors, errorStr);
-            }
-            if(formalParameters != NULL){//dividir el if para diferenciar el error
-                int errorStrLength = snprintf(NULL, 0, "\033[1;36mLine: %d \033[1;31m Error:\033[0m Miss parameters in function %s call\n", (ast->data)->lineNumber, methodName);
-                char* errorStr = malloc(errorStrLength + 1);
-                sprintf(errorStr, "\033[1;36mLine: %d \033[1;31m Error:\033[0m Miss parameters in function %s call\n", (ast->data)->lineNumber, methodName);
-                insertErrorNode(errors, errorStr);
-            }
+
+            if(currentParameters != NULL)
+                addMethodCallError(errors, (ast->data)->lineNumber, "To many parameters", methodName);
+
+            if(formalParameters != NULL)
+                addMethodCallError(errors, (ast->data)->lineNumber, "Miss parameters", methodName);
         }
     }
     checkForTypeErrors(ast->ls, errors);
     checkForTypeErrors(ast->rs, errors);
-}
-
-enum TType getAstType(TAst* ast) { 
-    if(ast==NULL) return NONETYPE;
-    if ((ast->data)->type != NONETYPE) {
-        return (ast->data)->type;
-    }
-    enum TType lsType = getAstType(ast->ls);
-    enum TType rsType = getAstType(ast->rs);
-    if (lsType != rsType) {
-        return ERROR;
-    }
-    (ast->data)->type = lsType;
-    return lsType;
-
 }
