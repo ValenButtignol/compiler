a6b81ada87f2dc7bddcfb090e2a73b8992a840ed
ValenButtignol <valentinbuttignol@gmail.com>
2023-11-08 17:33:27 -0300

    Fix test factory for new grammar rules

--- tests/utils/src/checkTypesInputs.c
+++ tests/utils/src/checkTypesInputs.c
@@ -37,143 +37,143 @@ int checkTypesFactory(char* inputTestFileName, ErrorNode** expectedErrors) {
 }
 
 void checkTypesListErrors1(ErrorNode** expectedErrors) {
-    addOperationError(expectedErrors, 5, ASSIGNMENT);
     addOperationError(expectedErrors, 3, ASSIGNMENT);
+    addOperationError(expectedErrors, 5, ASSIGNMENT);
 } 
 
 void checkTypesListErrors2(ErrorNode** expectedErrors) {
-    addOperationError(expectedErrors, 17, ASSIGNMENT);
-    addOperationError(expectedErrors, 16, ASSIGNMENT);
-    addOperationError(expectedErrors, 16, AND);
-    addOperationError(expectedErrors, 15, ASSIGNMENT);
-    addOperationError(expectedErrors, 15, AND);
-    addOperationError(expectedErrors, 14, ASSIGNMENT);
-    addOperationError(expectedErrors, 14, AND);
-    addOperationError(expectedErrors, 10, ASSIGNMENT);
-    addOperationError(expectedErrors, 9, ASSIGNMENT);
-    addOperationError(expectedErrors, 9, AND);
-    addOperationError(expectedErrors, 8, ASSIGNMENT);
-    addOperationError(expectedErrors, 8, AND);
+    addOperationError(expectedErrors, 3, ADD);
+    addOperationError(expectedErrors, 4, ADD);
+    addOperationError(expectedErrors, 5, ADD);
     addOperationError(expectedErrors, 7, ASSIGNMENT);
     addOperationError(expectedErrors, 7, AND);
-    addOperationError(expectedErrors, 5, ADD);
-    addOperationError(expectedErrors, 4, ADD);
-    addOperationError(expectedErrors, 3, ADD);
+    addOperationError(expectedErrors, 8, ASSIGNMENT);
+    addOperationError(expectedErrors, 8, AND);
+    addOperationError(expectedErrors, 9, ASSIGNMENT);
+    addOperationError(expectedErrors, 9, AND);
+    addOperationError(expectedErrors, 10, ASSIGNMENT);
+    addOperationError(expectedErrors, 14, ASSIGNMENT);
+    addOperationError(expectedErrors, 14, AND);
+    addOperationError(expectedErrors, 15, ASSIGNMENT);
+    addOperationError(expectedErrors, 15, AND);
+    addOperationError(expectedErrors, 16, ASSIGNMENT);
+    addOperationError(expectedErrors, 16, AND);
+    addOperationError(expectedErrors, 17, ASSIGNMENT);
 
 } 
 
 void checkTypesListErrors3(ErrorNode** expectedErrors) {
-    addOperationError(expectedErrors, 15, AND);
-    addOperationError(expectedErrors, 14, AND);
-    addOperationError(expectedErrors, 13, AND);
-    addOperationError(expectedErrors, 10, AND);
-    addOperationError(expectedErrors, 9, AND);
-    addOperationError(expectedErrors, 8, AND);
-    addOperationError(expectedErrors, 6, ASSIGNMENT);
-    addOperationError(expectedErrors, 6, ADD);
-    addOperationError(expectedErrors, 5, ASSIGNMENT);
-    addOperationError(expectedErrors, 5, ADD);
+    addOperationError(expectedErrors, 3, ASSIGNMENT);
     addOperationError(expectedErrors, 4, ASSIGNMENT);
     addOperationError(expectedErrors, 4, ADD);
-    addOperationError(expectedErrors, 3, ASSIGNMENT);
+    addOperationError(expectedErrors, 5, ASSIGNMENT);
+    addOperationError(expectedErrors, 5, ADD);
+    addOperationError(expectedErrors, 6, ASSIGNMENT);
+    addOperationError(expectedErrors, 6, ADD);
+    addOperationError(expectedErrors, 8, AND);
+    addOperationError(expectedErrors, 9, AND);
+    addOperationError(expectedErrors, 10, AND);
+    addOperationError(expectedErrors, 13, AND);
+    addOperationError(expectedErrors, 14, AND);
+    addOperationError(expectedErrors, 15, AND);
 
 }
 
 void checkTypesListErrors4(ErrorNode** expectedErrors) {
-    addOperationError(expectedErrors, 10, ADD);
-    addOperationError(expectedErrors, 10, ADD);
-    addOperationError(expectedErrors, 9, ADD);
-    addOperationError(expectedErrors, 8, ADD);
-    addOperationError(expectedErrors, 8, ADD);
-    addOperationError(expectedErrors, 7, ADD);
-    addOperationError(expectedErrors, 7, ADD);
-    addOperationError(expectedErrors, 6, ADD);
-    addOperationError(expectedErrors, 5, ADD);
     addOperationError(expectedErrors, 4, ADD);
+    addOperationError(expectedErrors, 5, ADD);
+    addOperationError(expectedErrors, 6, ADD);
+    addOperationError(expectedErrors, 7, ADD);
+    addOperationError(expectedErrors, 7, ADD);
+    addOperationError(expectedErrors, 8, ADD);
+    addOperationError(expectedErrors, 8, ADD);
+    addOperationError(expectedErrors, 9, ADD);
+    addOperationError(expectedErrors, 10, ADD);
+    addOperationError(expectedErrors, 10, ADD);
 } 
 
 void checkTypesListErrors5(ErrorNode** expectedErrors) {
-    addOperationError(expectedErrors, 9, AND);
-    addOperationError(expectedErrors, 8, AND);
-    addOperationError(expectedErrors, 7, AND);
-    addOperationError(expectedErrors, 6, AND);
-    addOperationError(expectedErrors, 6, AND);
-    addOperationError(expectedErrors, 5, AND);
-    addOperationError(expectedErrors, 5, AND);
+    addOperationError(expectedErrors, 3, AND);
+    addOperationError(expectedErrors, 3, AND);
     addOperationError(expectedErrors, 4, AND);
-    addOperationError(expectedErrors, 3, AND);
-    addOperationError(expectedErrors, 3, AND);
+    addOperationError(expectedErrors, 5, AND);
+    addOperationError(expectedErrors, 5, AND);
+    addOperationError(expectedErrors, 6, AND);
+    addOperationError(expectedErrors, 6, AND);
+    addOperationError(expectedErrors, 7, AND);
+    addOperationError(expectedErrors, 8, AND);
+    addOperationError(expectedErrors, 9, AND);
 } 
 
 void checkTypesListErrors6(ErrorNode** expectedErrors) {
-    addOperationError(expectedErrors, 18, ADD);
-    addOperationError(expectedErrors, 18, ADD);
-    addOperationError(expectedErrors, 18, ADD);
-    addOperationError(expectedErrors, 17, ADD);
-    addOperationError(expectedErrors, 17, ADD);
-    addOperationError(expectedErrors, 16, ADD);
-    addOperationError(expectedErrors, 16, ADD);
-    addOperationError(expectedErrors, 15, ADD);
-    addOperationError(expectedErrors, 15, ADD);
-    addOperationError(expectedErrors, 15, ADD);
-    addOperationError(expectedErrors, 14, ADD);
-    addOperationError(expectedErrors, 14, ADD);
-    addOperationError(expectedErrors, 14, ADD);
-    addOperationError(expectedErrors, 13, ADD);
-    addOperationError(expectedErrors, 12, ADD);
-    addOperationError(expectedErrors, 12, ADD);
-    addOperationError(expectedErrors, 11, ADD);
-    addOperationError(expectedErrors, 11, ADD);
-    addOperationError(expectedErrors, 10, ADD);
-    addOperationError(expectedErrors, 10, ADD);
-    addOperationError(expectedErrors, 9, ADD);
-    addOperationError(expectedErrors, 9, ADD);
-    addOperationError(expectedErrors, 8, ADD);
-    addOperationError(expectedErrors, 8, ADD);
-    addOperationError(expectedErrors, 7, ADD);
-    addOperationError(expectedErrors, 6, ADD);
-    addOperationError(expectedErrors, 5, ADD);
     addOperationError(expectedErrors, 4, ADD);
+    addOperationError(expectedErrors, 5, ADD);
+    addOperationError(expectedErrors, 6, ADD);
+    addOperationError(expectedErrors, 7, ADD);
+    addOperationError(expectedErrors, 8, ADD);
+    addOperationError(expectedErrors, 8, ADD);
+    addOperationError(expectedErrors, 9, ADD);
+    addOperationError(expectedErrors, 9, ADD);
+    addOperationError(expectedErrors, 10, ADD);
+    addOperationError(expectedErrors, 10, ADD);
+    addOperationError(expectedErrors, 11, ADD);
+    addOperationError(expectedErrors, 11, ADD);
+    addOperationError(expectedErrors, 12, ADD);
+    addOperationError(expectedErrors, 12, ADD);
+    addOperationError(expectedErrors, 13, ADD);
+    addOperationError(expectedErrors, 14, ADD);
+    addOperationError(expectedErrors, 14, ADD);
+    addOperationError(expectedErrors, 14, ADD);
+    addOperationError(expectedErrors, 15, ADD);
+    addOperationError(expectedErrors, 15, ADD);
+    addOperationError(expectedErrors, 15, ADD);
+    addOperationError(expectedErrors, 16, ADD);
+    addOperationError(expectedErrors, 16, ADD);
+    addOperationError(expectedErrors, 17, ADD);
+    addOperationError(expectedErrors, 17, ADD);
+    addOperationError(expectedErrors, 18, ADD);
+    addOperationError(expectedErrors, 18, ADD);
+    addOperationError(expectedErrors, 18, ADD);
 
 } 
 
 void checkTypesListErrors7(ErrorNode** expectedErrors) {
-    addOperationError(expectedErrors, 17, AND);
-    addOperationError(expectedErrors, 16, AND);
-    addOperationError(expectedErrors, 15, AND);
-    addOperationError(expectedErrors, 14, AND);
-    addOperationError(expectedErrors, 13, AND);
-    addOperationError(expectedErrors, 13, AND);
-    addOperationError(expectedErrors, 12, AND);
-    addOperationError(expectedErrors, 12, AND);
-    addOperationError(expectedErrors, 11, AND);
-    addOperationError(expectedErrors, 11, AND);
-    addOperationError(expectedErrors, 10, AND);
-    addOperationError(expectedErrors, 10, AND);
-    addOperationError(expectedErrors, 9, AND);
-    addOperationError(expectedErrors, 9, AND);
-    addOperationError(expectedErrors, 8, AND);
-    addOperationError(expectedErrors, 7, AND);
-    addOperationError(expectedErrors, 7, AND);
-    addOperationError(expectedErrors, 7, AND);
-    addOperationError(expectedErrors, 6, AND);
-    addOperationError(expectedErrors, 6, AND);
-    addOperationError(expectedErrors, 6, AND);
-    addOperationError(expectedErrors, 5, AND);
-    addOperationError(expectedErrors, 5, AND);
-    addOperationError(expectedErrors, 4, AND);
-    addOperationError(expectedErrors, 4, AND);
     addOperationError(expectedErrors, 3, AND);
     addOperationError(expectedErrors, 3, AND);
     addOperationError(expectedErrors, 3, AND);  
+    addOperationError(expectedErrors, 4, AND);
+    addOperationError(expectedErrors, 4, AND);
+    addOperationError(expectedErrors, 5, AND);
+    addOperationError(expectedErrors, 5, AND);
+    addOperationError(expectedErrors, 6, AND);
+    addOperationError(expectedErrors, 6, AND);
+    addOperationError(expectedErrors, 6, AND);
+    addOperationError(expectedErrors, 7, AND);
+    addOperationError(expectedErrors, 7, AND);
+    addOperationError(expectedErrors, 7, AND);
+    addOperationError(expectedErrors, 8, AND);
+    addOperationError(expectedErrors, 9, AND);
+    addOperationError(expectedErrors, 9, AND);
+    addOperationError(expectedErrors, 10, AND);
+    addOperationError(expectedErrors, 10, AND);
+    addOperationError(expectedErrors, 11, AND);
+    addOperationError(expectedErrors, 11, AND);
+    addOperationError(expectedErrors, 12, AND);
+    addOperationError(expectedErrors, 12, AND);
+    addOperationError(expectedErrors, 13, AND);
+    addOperationError(expectedErrors, 13, AND);
+    addOperationError(expectedErrors, 14, AND);
+    addOperationError(expectedErrors, 15, AND);
+    addOperationError(expectedErrors, 16, AND);
+    addOperationError(expectedErrors, 17, AND);
 } 
 
 void checkTypesListErrors8(ErrorNode** expectedErrors) {
 
     addMethodCallError(expectedErrors, 3, "Type mismatch", "foo");
     addMethodCallError(expectedErrors, 3, "Type mismatch", "foo");
-    addOperationError(expectedErrors, 11, NEGATIVE);
     addOperationError(expectedErrors, 10, NOT);
+    addOperationError(expectedErrors, 11, NEGATIVE);
 } 
 
 void checkTypesListErrors9(ErrorNode** expectedErrors) {
